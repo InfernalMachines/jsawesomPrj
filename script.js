c388e0de0a6f4fea14ef0151bf101bddb8520d1b
Diego <diego.ma.martinez@gmail.com>
2016-03-14 15:05:34 -0400

    obj literal

---
+++ script.js
@@ -0,0 +1,11 @@
+//obj literals 
+//Quick and dirty method to describe an object
+
+var apple {
+	color:"red";
+	worms:"false";
+	taste:"crisp";
+	rotton: function(){
+		return " Do not eat please!!! "
+	}
+};

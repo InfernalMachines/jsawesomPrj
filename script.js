bc3ac8aa2b8b0b94384b2ad0211243c0e4898a4b
Nancy Blayney <nblayney@g.cofc.edu>
2016-03-14 15:29:23 -0400

    prototypical objects and inheritance

--- script.js
+++ script.js
@@ -26,4 +26,39 @@ var pear = new Fruit("pear", "crisp", "round");
 banana.size 
 banana.pear
 banana.salad
-bannana["name"]
+banana["name"]
+
+//An object can inherit traits/attributes from a parent object using prototypical objects
+
+function Holiday(date, season, theme){
+	this.date = date;
+	this.season = season;
+	this.theme = theme;
+	this.party = function(){
+		return "come out on " + this.date + " to celebrate... " + this.theme + "-style";
+	}
+}
+// You can instantiate a new instance using the object
+var turkeyDay = new Holiday("Some thursday in november", "fall", "pilgrim")
+
+//You can also extend the parent object to a child object. In the following example, Independence Day is categorized as its own special prototype (class) of object with an additional arguments passed in which specifies the country
+function IndependenceDay(date, season, theme, country){
+	this.country = country;
+	Holiday.apply(this,arguments);
+}
+//This aligns the prototype of the child object with that of the parent object, so that any changes made to the parent object will be available in the child object as well.
+IndependenceDay.prototype = new Holiday();
+
+//here are some instances of the Independence Day object which extends the Holiday object
+var theFourth = new IndependenceDay("July 4th", "summer", "freedom", "'MURICA");
+var elCinco = new IndependenceDay("Cinco de Mayo", "spring", "rememberance", "Mexico");
+
+
+
+
+
+
+
+
+
+

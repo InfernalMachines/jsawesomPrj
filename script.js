fff05954efcce45341c0d53b1e3e39fa715fd641
Diego <diego.ma.martinez@gmail.com>
2016-03-14 15:08:02 -0400

    fixed bugs

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 //obj literals 
 //Quick and dirty method to describe an object
-
-var apple {
-	color:"red";
-	worms:"false";
-	taste:"crisp";
+var apple = {
+	color:"red",
+	worms:"false",
+	taste:"crisp",
 	rotton: function(){
 		return " Do not eat please!!! "
 	}
